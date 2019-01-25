bdbf3a80f1bfbbca728a15a7c5d22987db94ad3e
kachind <42634028+kachind@users.noreply.github.com>
2019-01-25 10:48:03 -0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while getopts :h:p:a:w:c: option
+do
+case "${option}"
+in
+h) HOST=${OPTARG};;
+p) PORT=${OPTARG};;
+a) ADDRESS=${OPTARG};;
+w) WORKER=${OPTARG};;
+t) CPU=${OPTARG};;
+esac
+done
+
+if [ "$CPU" -eq 0 ];then
+  CPU=$((`getconf _NPROCESSORS_ONLN`))
+fi
+
+if [ "$WORKER" -eq "KachInd" ];then
+  WORKER=$HOSTNAME
+fi
+
+./nheqminer -v -l "${HOST}":"${PORT}" -u "${ADDRESS}"."${WORKER}" -t "${CPU}" "$@"
