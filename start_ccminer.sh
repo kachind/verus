22358e65abcdcba45699179f110fb12433da0b99
kachind <42634028+kachind@users.noreply.github.com>
2019-02-09 15:57:39 -0800

    Create start_ccminer.sh

---
+++ start_ccminer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+while getopts "h:p:a:w:t:" option
+do
+case "${option}"
+in
+h) HOST=${OPTARG};;
+p) PORT=${OPTARG};;
+a) ADDRESS=${OPTARG};;
+w) WORKER=${OPTARG};;
+t) THREADS=${OPTARG};;
+esac
+done
+
+if [ "$THREADS" -eq 0 ];then
+  THREADS=$((`getconf _NPROCESSORS_ONLN`))
+fi
+
+if [ "$WORKER" = "KachInd" ];then
+  WORKER=$HOSTNAME
+fi
+
+echo "$HOST:$PORT $ADDRESS $WORKER $THREADS"
+./ccminer -a verus -o stratum:tcp://"${HOST}":"${PORT}" -u "${ADDRESS}"."${WORKER}" -t "${THREADS}"
