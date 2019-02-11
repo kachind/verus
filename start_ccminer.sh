d2c152bf3e9d98caad6662d563ec8583f9ec5392
kachind <42634028+kachind@users.noreply.github.com>
2019-02-11 15:42:11 -0800

    Update start_ccminer.sh

--- start_ccminer.sh
+++ start_ccminer.sh
@@ -20,5 +20,5 @@ if [ "$WORKER" = "KachInd" ];then
   WORKER=$HOSTNAME
 fi
 
-echo "$HOST:$PORT $ADDRESS $WORKER $THREADS"
+echo "-a verus -o stratum:tcp://$HOST:$PORT -u $ADDRESS.$WORKER -t THREADS"
 ./ccminer -a verus -o stratum:tcp://"${HOST}":"${PORT}" -u "${ADDRESS}"."${WORKER}" -t "${THREADS}"
